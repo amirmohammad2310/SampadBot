6d6e54ddf931dac1208ef80486f71ad32271238a
amir mohammad <amir84mo84@gmail.com>
2024-07-28 17:54:41 +0330

    start in start menu

--- Main.py
+++ Main.py
@@ -7,10 +7,23 @@ TOKEN = "7287866527:AAH0jFYht54U_XMCzc_ThdcQ-KSgK9f2yHE"
 
 bot = telebot.TeleBot(TOKEN)
 
+current_menu = 'start'
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	
+
+
+
+
+@bot.message_handler(commands = ['start'])
+def start_menu(message):
+	if(current_menu == 'start')
+		bot.reply_to(message, "سلام خوبی؟ \n به ربات کانون خوش اومدی")
+	bot.send_message(message.chat.id , "خوب حتما میخوای بدونی این ربات چیکار میکنه بزار تا بهت بگم\nto be complete")
+
+
+
+
+
 
 
 bot.infinity_polling()
