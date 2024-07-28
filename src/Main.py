dd9a3bf96d663e074487ccff05514739faec5471
amir mohammad <amir84mo84@gmail.com>
2024-07-29 01:11:38 +0330

    fix the multy thread bugs and sync the objects

--- src/Main.py
+++ src/Main.py
@@ -1,47 +1,71 @@
 import telebot
 import User
-import os
 from telebot import types
 
-directories = [entry for entry in os.listdir('.') if os.path.isdir(entry)]
 
-
-print("hello\n\n\n\n\n\n\n")
-print(directories)
 TOKEN = "7287866527:AAH0jFYht54U_XMCzc_ThdcQ-KSgK9f2yHE"
 
 
 bot = telebot.TeleBot(TOKEN)
 
-current_menu = 'start'
+
+
+
+@bot.message_handler(func=lambda message: True)
+def message_handle(message):
+	if(User.get_client_by_id(message.chat.id) == False):
+		current_menu = 'start'
+	else:
+		client = User.get_client_by_id(message.chat.id)
+		current_menu =client.current_menu
+	if(message.text == '/start'):
+		start_menu(message)
+	elif(message.text == '/help'):
+		show_help(message)
+	elif(message.text == '/register'):
+		register_menu(message)
+	elif(current_menu == 'firstname'):
+		firstname_input(message)
 
 	
 
 
 
 
-@bot.message_handler(commands = ['start'])
-def start_menu(message):
-	if(current_menu == 'start'):
-		bot.reply_to(message, "سلام خوبی؟ \n به ربات کانون خوش اومدی")
-	bot.send_message(message.chat.id , "خوب حتما میخوای بدونی این ربات چیکار میکنه بزار تا بهت بگم\nto be complete")
 
-@bot.message_handler(commands = ['help'])
+
+
+
+def start_menu(message):
+	bot.reply_to(message, "سلام خوبی؟ \n به ربات کانون خوش اومدی")
+	bot.send_message(message.chat.id , "خوب حتما میخوای بدونی این ربات چیکار میکنه بزار تا بهت بگم\nto be complete")
+	User.client(message.chat.id)
+	
+
+
+
+
 def show_help(message):
 	bot.reply_to(message , "عه مشکلی پیش اومده؟")
 	last_sent_message = bot.send_message(message.chat.id , "خوب بزار یه لیست از دستورات بهت بدم ")
 	last_sent_message = bot.reply_to(last_sent_message , "the commands lilst")
 	bot.reply_to(last_sent_message ,"هنوز مشکلت حل نشده ؟ قصه نداره به پشتیبانی فنی به آیدی \"@amirmo844\" پیام بده" )
 
-@bot.message_handler(commands = ['register'])
 def register_menu(message):
 	id = message.chat.id
-	if(User.get_user_json(id)):
-		bot.reply_to(message , "there is a account")
+	if(User.is_user_json(id)):
+		information = User.get_user_json(id)
+		bot.reply_to(message , information.get("firstname") )
+	
 	else:
-	    User.save_user(User.make_json_string("amir" ,"sdffas" , "sdffas", "sdffas", "sdffas", "sdffas", "sdffas"), message.chat.id)
-	    bot.reply_to(message , "okey you signed up")
-
+		client = User.get_client_by_id(message.chat.id)
+		client.current_menu = "firstname"
+		bot.reply_to(message , "enter your first name")
+	    
+		
+		
+def firstname_input(message):
+	bot.reply_to(message , message.text)
 	
 
 	
--- src/User.py
+++ src/User.py
@@ -1,6 +1,33 @@
 import json
 import os.path
 
+global clients
+global clients_id
+clients = set()
+clients_id = set()
+
+class client:
+    def __init__(self , id):
+        self.id = id
+        clients.add(self)
+        clients_id.add(id)
+    firstname = None
+    current_menu = "start"
+    
+
+
+def get_client_by_id(id):
+        if(id in clients_id ):
+            for item in clients:
+                if(id == item.id):
+                    return item
+        else:
+            return False
+
+
+
+
+
 def make_json_string(firstname , lastname , phone_number , graduation_year  , field_of_study, id , password ):
     return json.dumps({"firstname" :firstname ,
                        'lastname' : lastname ,
@@ -16,9 +43,11 @@ def save_user(json_of_user , id):
     file.write(json_of_user)
     file.close()
 
-def get_user_json(id):
+def is_user_json(id):
     if(os.path.exists("../data/" + str(id)+ ".json")):
         return True
     else :
         return False
-
+def get_user_json(id):
+    file = open("../data/" + str(id)+ '.json' , 'r')
+    return json.load(file)
