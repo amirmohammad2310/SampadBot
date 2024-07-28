eaa6b51bf18eec2a2468499483b65c15066edb68
amir mohammad <amir84mo84@gmail.com>
2024-07-28 18:14:14 +0330

    first help menu

--- Main.py
+++ Main.py
@@ -16,11 +16,20 @@ current_menu = 'start'
 
 @bot.message_handler(commands = ['start'])
 def start_menu(message):
-	if(current_menu == 'start')
+	if(current_menu == 'start'):
 		bot.reply_to(message, "سلام خوبی؟ \n به ربات کانون خوش اومدی")
 	bot.send_message(message.chat.id , "خوب حتما میخوای بدونی این ربات چیکار میکنه بزار تا بهت بگم\nto be complete")
 
+@bot.message_handler(commands = ['help'])
+def show_help(message):
+	bot.reply_to(message , "عه مشکلی پیش اومده؟")
+	last_sent_message = bot.send_message(message.chat.id , "خوب بزار یه لیست از دستورات بهت بدم ")
+	last_sent_message = bot.reply_to(last_sent_message , "the commands lilst")
+	bot.reply_to(last_sent_message ,"هنوز مشکلت حل نشده ؟ قصه نداره به پشتیبانی فنی به آیدی \"@amirmo844\" پیام بده" )
 
+
+
+	
 
 
 
