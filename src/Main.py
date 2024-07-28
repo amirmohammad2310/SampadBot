3d5cf897153399e7f0da1acb6b5a1e7cf2f254f8
amir mohammad <amir84mo84@gmail.com>
2024-07-29 03:28:23 +0330

    complete the register menu

--- src/Main.py
+++ src/Main.py
@@ -1,6 +1,6 @@
 import telebot
 import User
-from telebot import types
+import re
 
 
 TOKEN = "7287866527:AAH0jFYht54U_XMCzc_ThdcQ-KSgK9f2yHE"
@@ -26,13 +26,21 @@ def message_handle(message):
 		register_menu(message)
 	elif(current_menu == 'firstname'):
 		firstname_input(message)
-
+	elif(current_menu == 'lastname'):
+		lastname_input(message)
+	elif(current_menu == 'phone_number'):
+		phone_number_input(message)
+	elif(current_menu == 'graduation'):
+		graduation_year_input(message)
+	elif(current_menu == 'field'):
+		field_of_study_input(message)
+	elif(current_menu == 'password'):
+		password_input(message)
+	elif(current_menu == 'repassword'):
+		re_password_input(message)
 	
 
-
-
-
-
+	
 
 
 
@@ -41,10 +49,6 @@ def start_menu(message):
 	bot.send_message(message.chat.id , "خوب حتما میخوای بدونی این ربات چیکار میکنه بزار تا بهت بگم\nto be complete")
 	User.client(message.chat.id)
 	
-
-
-
-
 def show_help(message):
 	bot.reply_to(message , "عه مشکلی پیش اومده؟")
 	last_sent_message = bot.send_message(message.chat.id , "خوب بزار یه لیست از دستورات بهت بدم ")
@@ -53,21 +57,74 @@ def show_help(message):
 
 def register_menu(message):
 	id = message.chat.id
-	if(User.is_user_json(id)):
-		information = User.get_user_json(id)
-		bot.reply_to(message , information.get("firstname") )
+	if(User.is_user_exist_with_id(id)):
+		information = User.get_user_json_from_file(id)
+		bot.reply_to(message , f"شما قبلا با شماره تماس {information.['phone_number']}" )
 	
 	else:
 		client = User.get_client_by_id(message.chat.id)
 		client.current_menu = "firstname"
 		bot.reply_to(message , "enter your first name")
-	    
-		
-		
+	    		
 def firstname_input(message):
-	bot.reply_to(message , message.text)
+	client = User.get_client_by_id(message.chat.id)
+	client.firstname = message.text
+	bot.reply_to(message , f'نام کوچک شما {message.text} ثبت شد')
+	bot.send_message(client.id , "لطفا فامیلی تو بنویس")
+	client.current_menu = 'lastname'
 	
+def lastname_input(message):
+	client = User.get_client_by_id(message.chat.id)
+	client.lastname = message.text
+	bot.reply_to(message , f'نام فامیلی شما {message.text} ثبت شد')
+	bot.send_message(client.id , "لطفا شماره تلفنتو بنویس")
+	client.current_menu = 'phone_number'
 
+def phone_number_input(message):
+	if((User.is_phone_number_valid(message.text)) == False):
+		bot.send_message(message.chat.id , "لطفا یک شماره تلفن همراه معتبر به همراه صفر در اولش بنویسید")
+	else:
+		client = User.get_client_by_id(message.chat.id)
+		client.phone_number = message.text
+		bot.reply_to(message , f'شماره تماس شما {message.text} ثبت شد')
+		bot.send_message(client.id , "لطفا سال فارغ التحصیلیت رو بنویس")
+		client.current_menu = 'graduation'
+
+def graduation_year_input(message):
+	if((User.is_graduation_valid(message.text)) == False):
+		bot.send_message(message.chat.id , "لطفا یک عدد چهار رقمی درست وارد کن")
+	else:
+		client = User.get_client_by_id(message.chat.id)
+		client.graduation_year = message.text
+		bot.reply_to(message , f'زمان فارغ التحصیلی  شما {message.text} ثبت شد')
+		bot.send_message(client.id , "لطفا رشته التحصیلیت رو بنویس")
+		client.current_menu = 'field'
+
+def field_of_study_input(message):
+	client = User.get_client_by_id(message.chat.id)
+	client.field_of_study = message.text
+	bot.reply_to(message , f'رشته تحصیلی شما {message.text} ثبت شد')
+	bot.send_message(client.id , "لطفا رمز عبورتو بنویس بنویس")
+	client.current_menu = 'password'
+
+def password_input(message):
+	client = User.get_client_by_id(message.chat.id)
+	client.password = message.text
+	bot.send_message(client.id , "لطفا تکرار رمز عبورتو بنویس بنویس")
+	client.current_menu = 'repassword'
+
+def re_password_input(message):
+	client = User.get_client_by_id(message.chat.id)
+	if(client.password != message.text):
+		bot.send_message(message.chat.id , "رمز عبور با رمز عبور اولیه مطابقت نداره")
+	else:
+		bot.reply_to(message , f'رمز عبور   شما {message.text} ثبت شد')
+		User.save_client_to_database(client)
+		bot.send_message(client.id , "ثبت نام شما کامل شد و اطلاعات شما ثبت شد")
+		client.current_menu="main_menu"
+
+def log_out_menu(message):
+	
 	
 
 
--- src/User.py
+++ src/User.py
@@ -1,5 +1,6 @@
 import json
 import os.path
+import re
 
 global clients
 global clients_id
@@ -11,7 +12,12 @@ class client:
         self.id = id
         clients.add(self)
         clients_id.add(id)
-    firstname = None
+    firstname = str()
+    lastname = str()
+    phone_number = str()
+    graduation_year = str()
+    field_of_study = str()
+    password = str()
     current_menu = "start"
     
 
@@ -26,7 +32,9 @@ def get_client_by_id(id):
 
 
 
-
+def save_client_to_database(client):
+    s = make_json_string(client.firstname , client.lastname , client.phone_number , client.graduation_year , client.field_of_study , client.id , client.password)
+    save_user(s , client.id , client.phone_number)
 
 def make_json_string(firstname , lastname , phone_number , graduation_year  , field_of_study, id , password ):
     return json.dumps({"firstname" :firstname ,
@@ -38,16 +46,39 @@ def make_json_string(firstname , lastname , phone_number , graduation_year  , fi
                        'password' : password})
 
 
-def save_user(json_of_user , id):
-    file = open("../data/" + str(id)+ '.json' , 'w')
+def save_user(json_of_user , id , phone_number):
+    file = open("../data/" +str(phone_number)+'_'+ str(id)+ '.json' , 'w')
     file.write(json_of_user)
     file.close()
 
-def is_user_json(id):
+
+
+
+def is_user_exist_with_phone_number(phone_number):
     if(os.path.exists("../data/" + str(id)+ ".json")):
         return True
     else :
         return False
-def get_user_json(id):
+def get_user_json_from_file(id):
     file = open("../data/" + str(id)+ '.json' , 'r')
     return json.load(file)
+
+
+def is_phone_number_valid(number):
+    if(len(number) != 11):
+        return False
+    if(re.search("09\d{9}" , number) == None):
+        return False
+    if(re.search("09\d{9}" , number).start() != 0):
+        return False
+    return True
+
+
+def is_graduation_valid(number):
+    if(len(number) != 4):
+        return False
+    if(re.search("1\d{3}" , number) == None):
+        return False
+    if(re.search("1\d{3}" , number).start() != 0):
+        return False
+    return True
